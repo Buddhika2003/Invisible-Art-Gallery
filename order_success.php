ebd6d40e6ec5d2607b7110fc24df2d340e7c7b3d
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-07 23:08:15 +0530

    create a page for finalize the order.This is for order recieved

---
+++ order_success.php
@@ -0,0 +1,16 @@
+<?php
+    include 'header.php';
+?>
+
+<main class = "wrap">
+    <h2>Order Successful</h2>
+    <p>Thank you for your order! Your custom portrait 
+        request has been received successfully. We will
+         contact you shortly with further details.</p>
+
+         <a href ="index.php" class="btn">Return to Home</a>
+</main>
+
+<?php
+    include 'footer.php';
+?>
